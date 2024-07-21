4c382122eb22a93a5967815a945f189524e84ffe
Chih-Pin Tan <tanchihpin0517@gmail.com>
2024-07-21 16:24:19 +0000

    Remove the auto-export of the infer function

--- sheetsage/__init__.py
+++ sheetsage/__init__.py
@@ -15,8 +15,3 @@ def load_tests(loader, standard_tests, pattern):
     package_tests = loader.discover(start_dir=LIB_DIR, pattern="*test.py")
     standard_tests.addTests(package_tests)
     return standard_tests
-
-
-from .infer import sheetsage as infer
-
-__all__ = ["infer"]
